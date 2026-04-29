994017fc87cc79b709e0dbd421ab04d466944ad4
darkhorse9372 <darkhorse9372@gmail.com>
2026-04-30 01:13:03 +0800

    Update Conversaction

--- components/Conversation.tsx
+++ components/Conversation.tsx
@@ -1,11 +1,25 @@
 import AgentMessage from "./AgentMesssage.tsx";
 import UserMessage from "./UserMessage.tsx";
+import { SiteMap } from "../enums/siteMap.ts";
+import { BrainChatMessage } from "../types/brain.ts";
 
-export default function Conversation({userMessage, agentMessage}: {userMessage: string, agentMessage:string}) {
-	return (
-		<div class="flex flex-col items-start">
-			<UserMessage userMessage={userMessage} />
-			<AgentMessage agentMessage={agentMessage} />
-		</div>
-	);
+export default function Conversation(
+  { brainName, message }: { brainName: string; message: BrainChatMessage },
+) {
+  return (
+    <div class="flex flex-col items-start">
+      <UserMessage userMessage={message.userMessage} />
+      <AgentMessage agentMessage={message.agentMessage} />
+      <form method="POST" action={SiteMap.CONVERSATION}>
+        <input type="hidden" name="brainName" value={brainName} />
+        <input type="hidden" name="messageId" value={message.id} />
+        <button
+          type="submit"
+          class="text-brain-pink font-chakra text-sm pb-3 hover:text-brain-text"
+        >
+          Delete this part
+        </button>
+      </form>
+    </div>
+  );
 }
